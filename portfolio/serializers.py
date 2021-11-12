78014d4e600046105007485aead96bc8c8ba7166
Huang Kai <huangkai@Huangs-MacBook-Pro.local>
2021-11-12 14:25:57 +0800

    added function to make json response

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Portfolio
 
 class PortfolioSerializer(serializers.ModelSerializer):
-
+    
     class Meta:
         model = Portfolio 
         fields = '__all__'
+
+    def to_representation(self, instance):
+        row = super(PortfolioSerializer, self).to_representation(instance)
+        return {row["id"]:row}
